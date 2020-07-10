a1f300ba2267e8f122d87599c7040597545cad60
Darker <justdarker@gmail.com>
2020-07-11 02:30:29 +1000

    Initial commit

---
+++ gendiff/diffs/diff.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate difference of two files')
+    args = parser.parse_args()
+
+    
+    
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from gendiff.diffs import diff
+
+def main():
+    diff.main()
+
+
+if __name__ == '__main__':
+    main()
